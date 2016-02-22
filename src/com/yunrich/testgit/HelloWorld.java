ebaae90e00014aab20e0033b4573ecf95c47c686
hugan <2512581781@qq.com>
2016-02-22 09:46:10 +0800

    首次提交

---
+++ src/com/yunrich/testgit/HelloWorld.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ * 上海云之富金融信息服务有限公司
+ * Copyright (c) 2014-2016 YunCF,Inc.All Rights Reserved.
+ */
+package com.yunrich.testgit;
+
+/**
+ * 
+ * @author Scofield.hu
+ * @version $Id: HelloWorld.java, v 0.1 2016-2-19 下午05:16:58 Scofield.hu Exp $
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
